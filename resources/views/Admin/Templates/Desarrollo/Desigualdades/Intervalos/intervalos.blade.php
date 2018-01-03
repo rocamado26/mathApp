8c2a4fb23ad61a610e297036aac9fc28a59bf675
Merino <merino.tito@treming.com>
2018-01-03 00:00:05 -0600

    grafico intervalos

--- resources/views/Admin/Templates/Desarrollo/Desigualdades/Intervalos/intervalos.blade.php
+++ resources/views/Admin/Templates/Desarrollo/Desigualdades/Intervalos/intervalos.blade.php
@@ -43,16 +43,17 @@
                                                 <br>Los intervalos finitos pueden ser cerrados, abiertos o semiabiertos.
                                                 Sean <b> a </b>y <b>b</b> dos números reales tales que <b> a < b</b>.
                                                 <br><br>
-                                                <strong>Intervalo cerrado</strong><br>
-                                                Es el conjunto de números reales formado por a, b y todos los comprendidos entre ambos.
+                                                <strong>Intervalo cerrado [ m, n ]</strong><br>
+                                                <img src="img/iconos/desigualdades/desarrollo/intervalos/intervalo-c.PNG" class="img-responsive">
+                                                <br>
+                                                <strong>Intervalo abierto ] m, n [</strong><br>
+                                                <img src="img/iconos/desigualdades/desarrollo/intervalos/intervalo-a-a.PNG" class="img-responsive">
+                                                <br>
+                                                <strong>Intervalo semiabierto a izquierda (o semicerrado a derecha) ] m, n ]</strong><br>
+                                                <img src="/img/iconos/desigualdades/desarrollo/intervalos/intervalo-a-c.PNG" class="img-responsive">
                                                 <br><br>
-                                                <strong>Intervalo cerrado</strong><br>
-                                                Es el conjunto de los números reales comprendidos entre a y b. <br><br>
-                                                <strong>Intervalo semiabierto a izquierda (o semicerrado a derecha)</strong><br>
-                                                Es el conjunto de números reales formado por b y los números comprendidos entre a y b.
-                                                <br><br>
-                                                <strong>Intervalo semiabierto a derecha (o semicerrado a izquierda)</strong><br>
-                                                Es el conjunto de números reales formado por a y los números comprendidos entre a y b.
+                                                <strong>Intervalo semiabierto a derecha (o semicerrado a izquierda) [ m, n [</strong><br>
+                                                <img src="/img/iconos/desigualdades/desarrollo/intervalos/intervalo-c-i.PNG" class="img-responsive">
                                                 <br><br>
                                                 <strong></strong><br>
                                             </p>
@@ -66,7 +67,13 @@
                                                         </div>
                                                         <div class="panel-body">
                                                             <p style="text-align: justify;">
-
+                                                                <b>Menor que " < "</b> : x < 5: x es menor que 5, todos los menores que 5 sin incluir a 5
+                                                                <br><br>
+                                                                <b>Menor igual que " ≤ " </b>: x ≤ 8: x es menor o igual que 8, todos los menores que 8 incluyendo a 8
+                                                                <br><br>
+                                                                <b>Mayor que " > " </b>: x > 3: x es mayor que 3, todos los mayores que 3 sin incluir a 3
+                                                                <br><br>
+                                                                <b>Mayor igual que " ≤ "</b> : x ≤ 8: x es menor o igual que 8, todos los menores que 8 incluyendo a 8
                                                             </p>
                                                         </div>
                                                     </div>
@@ -116,7 +123,12 @@
                             </div>
                             <div id="collapseThree" class="panel-collapse collapse" aria-expanded="false">
                                 <div class="panel-body">
-
+                                    <div class="row">
+                                        <div class="col-md-6"></div>
+                                        <div class="col-md-6">
+                                            <div class="jxgbox" id="plano1" style="width: 300px;height: 300px; "></div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -126,3 +138,11 @@
         </div>
     </div>
 </div>
+<script>
+    brd = JXG.JSXGraph.initBoard('plano1',{boundingbox:[-5,5,5,-5], keepaspectratio:true, axis:true,showCopyright:false});
+    p1 = brd.createElement('point',[1,0],{visible:false});
+    p2 = brd.createElement('point',[-2,0],{visible:false});
+    var t1 = brd.create('text',[1,0.6," ] "],{fontSize:25,cssClass:""});
+    var t2 = brd.create('text',[-2,0.6," [ "],{fontSize:25,cssClass:""});
+    var l1 = brd.createElement('line', [p1,p2],{straightFirst:false, straightLast:false, strokeWidth:2,snapSizeX:5});
+</script>
